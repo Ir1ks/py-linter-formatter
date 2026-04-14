73907330b5c043835ed814ab1d20591d9c70679c
Maksym Samoilenko <maxit779@gmail.com>
2026-04-14 15:34:59 +0300

    delet blank lines

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(e) for e in errors],
@@ -17,10 +16,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     }
 
 
-
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(path, errs)
         for path, errs in linter_report.items()
     ]
-
